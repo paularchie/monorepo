45ccfce44c25dd4b8297f9a828c96aa7fa33845d
pawelluk <pawel@ember.co>
2022-09-11 11:33:30 +0200

    add storybook and button component

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -1,10 +1,43 @@
-/* eslint-disable-next-line */
-export interface ButtonProps {}
+import { Button as MantineButton, MantineColor } from '@mantine/core';
+import React from 'react';
 
-export const Button = (props: ButtonProps) => {
+type ButtonVariant = 'primary' | 'secondary';
+type ButtonType = 'button' | 'submit' | 'reset';
+
+type ButtonProps = {
+  text: string;
+  variant?: ButtonVariant;
+  type?: ButtonType;
+  disabled?: boolean;
+  onClick?: () => void;
+  leftIcon?: JSX.Element;
+  rightIcon?: JSX.Element;
+};
+
+const buttonColorMapping: Record<ButtonVariant, MantineColor> = {
+  primary: 'blue',
+  secondary: 'red',
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  variant = 'primary',
+  type = 'button',
+  text,
+  disabled,
+  onClick,
+  leftIcon,
+  rightIcon,
+}) => {
   return (
-    <div>
-      <h1>Welcome to Button</h1>
-    </div>
+    <MantineButton
+      type={type}
+      disabled={disabled}
+      color={buttonColorMapping[variant]}
+      onClick={onClick}
+      leftIcon={leftIcon}
+      rightIcon={rightIcon}
+    >
+      {text}
+    </MantineButton>
   );
 };
